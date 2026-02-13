edb984e6907c7d1b84ef0d8ed239ea1f9005aae9
erickzns <162044386+erickzns@users.noreply.github.com>
2026-02-13 13:30:17 -0300

    Create default configuration file for GhostMenu 2.0

---
+++ config.lua
@@ -0,0 +1,59 @@
+-- Configuration file for GhostMenu 2.0
+
+-- Aimbot settings
+aimbot = {
+    enabled = true,
+    aim_at = 'head',
+    smoothness = 5,
+    fov = 90,
+}
+
+-- ESP settings
+esp = {
+    enabled = true,
+    color = { r = 255, g = 0, b = 0 },
+    distance = 100,
+    show_health = true,
+}
+
+-- Weapon mods settings
+weapon_mods = {
+    rapid_fire = true,
+    no_recoil = true,
+    rapid_reload = false,
+}
+
+-- Player control settings
+player_control = {
+    speed_multiplier = 1.5,
+    jump_height = 10,
+}
+
+-- Visual settings
+visual = {
+    field_of_view = 90,
+    resolution = '1920x1080',
+}
+
+-- Bypass settings
+bypass = {
+    anti_cheat = true,
+}
+
+-- Performance settings
+performance = {
+    frame_rate_limit = 60,
+    graphics_quality = 'high',
+}
+
+-- Hotkeys settings
+hotkeys = {
+    toggle_aimbot = 'F1',
+    toggle_esp = 'F2',
+}
+
+-- Utility settings
+utility = {
+    logging_enabled = true,
+    debug_mode = false,
+}
